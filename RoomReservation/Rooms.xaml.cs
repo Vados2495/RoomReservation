c2c8c9c22f9ee2b30304b105b82d16215d9c405d
Vados2495 <117577379+Vados2495@users.noreply.github.com>
2023-04-07 01:25:26 +0300

    Данные таблицы Rooms

--- RoomReservation/Rooms.xaml.cs
+++ RoomReservation/Rooms.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +21,92 @@ namespace RoomReservation
     /// </summary>
     public partial class Rooms : Window
     {
+        private const string CONNECTION_STRING = "Data Source = DESKTOP-UBC2MQ9; Initial Catalog = Reservation; Trusted_Connection = True";
+        private const string SELECT_QUERY = "SELECT * FROM Rooms";
+        private SqlDataAdapter dataAdapter;
+        private DataTable dataTable;
         public Rooms()
         {
             InitializeComponent();
+            LoadData();
+        }
+        private void LoadData()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(CONNECTION_STRING))
+                {
+                    dataAdapter = new SqlDataAdapter(SELECT_QUERY, connection);
+                    SqlCommandBuilder commandBuilder = new SqlCommandBuilder(dataAdapter);
+                    dataTable = new DataTable();
+                    dataAdapter.Fill(dataTable);
+                    usersGrid.ItemsSource = dataTable.DefaultView;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to load data: {ex.Message}");
+            }
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            DataRow newRow = dataTable.NewRow();
+            dataTable.Rows.Add(newRow);
+        }
+
+        private void updateButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                dataAdapter.Update(dataTable);
+                LoadData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to update data: {ex.Message}");
+            }
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (usersGrid.SelectedItems != null && usersGrid.SelectedItems.Count > 0)
+            {
+                List<DataRowView> selectedRows = usersGrid.SelectedItems.Cast<DataRowView>().ToList();
+
+                if (MessageBox.Show($"Are you sure you want to delete {selectedRows.Count} rows?", "Confirmation", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                {
+                    try
+                    {
+                        foreach (DataRowView selectedRow in selectedRows)
+                        {
+                            DataRow row = selectedRow.Row;
+                            row.Delete();
+                        }
+
+                        dataAdapter.Update(dataTable);
+                        LoadData();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Failed to delete data: {ex.Message}");
+                    }
+                }
+            }
+        }
+
+        private void RoomsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            Rooms rooms = new Rooms();
+            rooms.Show();
+        }
+
+        private void BookingsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            Bookings bookings = new Bookings();
+            bookings.Show();
         }
     }
 }
