9b3ec356962389d18f411726db20e2930a219263
Vados2495 <117577379+Vados2495@users.noreply.github.com>
2023-05-25 10:49:35 +0300

    Realised MVVM pattern

--- RoomReservation/View/HotelBooking.xaml.cs
+++ RoomReservation/View/HotelBooking.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace RoomReservation
+namespace RoomReservation.View
 {
     /// <summary>
     /// Логика взаимодействия для HotelBooking.xaml
---
+++ RoomReservation/ViewModel/HotelBookingViewModel.cs
@@ -0,0 +1,56 @@
+﻿using RoomReservation.Model;
+using RoomReservation.View;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace RoomReservation.ViewModel
+{
+    public class HotelBookingViewModel : INotifyPropertyChanged
+    {
+        private List<Hotel> hotels;
+        private List<Room> rooms;
+        private BookingModel bookingModel;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public List<Hotel> Hotels
+        {
+            get { return hotels; }
+            set
+            {
+                hotels = value;
+                OnPropertyChanged(nameof(Hotels));
+            }
+        }
+
+        public List<Room> Rooms
+        {
+            get { return rooms; }
+            set
+            {
+                rooms = value;
+                OnPropertyChanged(nameof(Rooms));
+            }
+        }
+
+        public BookingModel BookingModel
+        {
+            get { return bookingModel; }
+            set
+            {
+                bookingModel = value;
+                OnPropertyChanged(nameof(BookingModel));
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
